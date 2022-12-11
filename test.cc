107f516095aa74bdb9aadf8eeb0f11d6ebd2ed99
Xiaoxu Guo <ftiasch0@gmail.com>
2022-12-11 15:51:09 +0800

    added more async

--- test.cc
+++ test.cc
@@ -3,11 +3,14 @@
 
 #include <algorithm>
 #include <execution>
+#include <future>
 #include <iostream>
 #include <latch>
 #include <stop_token>
 #include <thread>
 #include <vector>
+
+using namespace std::chrono_literals;
 
 using namespace Catch::Generators;
 
@@ -37,25 +40,63 @@ TEST_CASE("std::jthread") {
     std::cerr << "busy_wait|end" << std::endl;
   }};
   std::cerr << "main|begin" << std::endl;
-  std::this_thread::sleep_for(std::chrono::seconds(1));
+  std::this_thread::sleep_for(1s);
   std::cerr << "main|end" << std::endl;
 }
 
 TEST_CASE("std::latch") {
   const int THREADS = 2;
   std::latch done(THREADS);
-  std::cerr << "main|begin" << std::endl;
   std::vector<std::jthread> threads;
   for (int i = 0; i < THREADS; ++i) {
     threads.emplace_back(std::jthread([i, &done]() {
-      std::cerr << "thread" << i << "|begin" << std::endl;
       std::this_thread::sleep_for(std::chrono::seconds(i + 1));
-      std::cerr << "thread" << i << "|wake" << std::endl;
       done.count_down();
-      std::cerr << "thread" << i << "|end" << std::endl;
     }));
   }
-  std::cerr << "main|wait" << std::endl;
+  REQUIRE(!done.try_wait());
   done.wait();
-  std::cerr << "main|end" << std::endl;
+}
+
+TEST_CASE("std::async") {
+  auto f = std::async([]() -> int {
+    std::this_thread::sleep_for(1s);
+    return 233;
+  });
+  REQUIRE(f.wait_for(0s) != std::future_status::ready);
+  auto got = f.get();
+  REQUIRE(got == 233);
+}
+
+TEST_CASE("std::promise") {
+  std::promise<int> prom;
+  auto f = prom.get_future();
+  std::cerr << "main|begin" << std::endl;
+  /*
+   * We should give std::jthread a name
+   * to prevent it from deconstruction
+   */
+  std::jthread j([&prom](std::stop_token) -> void {
+    std::this_thread::sleep_for(1s);
+    prom.set_value(233);
+  });
+  REQUIRE(f.wait_for(0s) != std::future_status::ready);
+  auto got = f.get();
+  REQUIRE(got == 233);
+}
+
+TEST_CASE("std::packaged_task") {
+  std::packaged_task<int(int)> work([](int i) {
+    std::this_thread::sleep_for(1s);
+    return i;
+  });
+  auto f = work.get_future();
+  /*
+   * std::packaged_task should be involed in another thread
+   * Remember to std::move(work)
+   */
+  std::jthread j{std::bind(std::move(work), 233)};
+  REQUIRE(f.wait_for(0s) != std::future_status::ready);
+  auto got = f.get();
+  REQUIRE(got == 233);
 }
